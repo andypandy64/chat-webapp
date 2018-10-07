52d9f29227b6935fc7f533b1e2cbf8789cc85e2f
AndersPK <kamsvaag.anders@gmail.com>
2018-10-07 08:42:29 +0200

    Added more features

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ io.on('connection', (socket) => {
 
 io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
+    if (msg == '') return;
     let user = users.find((user) => user.id === socket.id);
     if (msg.charAt(0) == '/') {
       let command = msg.split(" ");
@@ -46,6 +47,24 @@ io.on('connection', (socket) => {
       }
     }
 
+    let isImage = /^https?:\/\/(?:[a-z0-9\-]+\.)+[a-z]{2,6}(?:\/[^\/#?]+)+\.(?:jpe?g|gif|png|bmp)$/i;
+    let isYoutubeUrl = /^(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?(?=.*v=((\w|-){11}))(?:\S+)?$/;
+    
+    let msgSplit = msg.split(' ');
+    let newMsg = msgSplit.map(word => {
+      if(isImage.test(word))
+        return '<img class="messageImage" src="' + word + '">';
+      else if (isYoutubeUrl.test(word)) {
+        let id = word.split("?v=")[1];
+        return '<iframe class="youtubeEmbed" src="http://www.youtube.com/embed/' + id + '" frameborder="0" allowfullscreen></iframe>';
+      }
+      return linkifyHtml(word, {
+        defaultProtocol: 'https'
+      });
+    });
+
+    msg = newMsg.join(' ');
+    
     io.emit('chat message', {
       username: user.username,
       message: msg,
@@ -57,6 +76,27 @@ io.on('connection', (socket) => {
 const sendUserList = s => {
   io.emit('userList update', users);
 }
+
+let usersTyping = [];
+let output = '';
+
+io.on('connection', socket => {
+  socket.on('user typing', () => {
+    let username = users.find(user => user.id == socket.id).username;
+    if (!usersTyping.includes(username)) usersTyping.push(username);
+  });
+});
+
+setInterval(() => {
+  activeUsersTyping = usersTyping.slice();
+  usersTyping = [];
+  activeUsersTyping.forEach(username => output += (username + ', '));
+  output = output.slice(0, -2);
+  if (activeUsersTyping.length)
+    output += (activeUsersTyping.length > 1) ? ' are typing...' : ' is typing...';
+  io.emit('users typing', output);
+  output = '';
+},2000);
 
 http.listen(process.env.PORT || 3000, () => {
   console.log('listening on *:3000');
